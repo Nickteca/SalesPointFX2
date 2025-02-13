e4762276bd296a8befb6e5cf0699fb5017f0eee4
Nickteca <isaaclunaavila@gmail.com>
2025-02-13 17:03:13 -0600

    Commit creando starter

---
+++ src/main/java/com/salespointfx2/www/controller/StarterController.java
@@ -0,0 +1,18 @@
+package com.salespointfx2.www.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StarterController {
+    @FXML
+    private BorderPane anchorPanePrincipal;
+
+    @FXML
+    private MenuItem mItemAbrirCajon;
+
+    @FXML
+    private MenuItem mItemCerrarCaja;
+
+    @FXML
+    private MenuItem mItemVenta;
+}
